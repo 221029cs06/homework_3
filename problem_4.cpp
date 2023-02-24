aefceebac7f258a53420b96ee2de712ff56301e6
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-02-24 23:11:07 +0300

    Update problem_4.cpp

--- problem_4.cpp
+++ problem_4.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 using namespace std;
-int main() {
-   char a;
-    cin >> a;
-    if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z'))
-    {
-        
-        if (a == 'a' || a == 'A' ||
-            a == 'e' || a == 'E' || 
-            a == 'u' || a == 'U' ||
-            a == 'O' || a == 'o' || 
-            a == 'I' || a == 'i')
-            cout  << "Vowel " << endl; 
-        else   cout << "Consonant" ; 
-    }
- 
+int main(){
+char sounds; 
+cin>>sounds; 
+switch (sounds)
+{
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'y':
+    cout<<"Vowel"<<endl;
+    break;
+    
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'f':
+    case 'g':
+    case 'h':
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm':
+    case 'n':
+    case 'p':  
+    case 'q':
+    case 'r':
+    case 's':
+    case 't':
+    case 'v':
+    case 'w':
+    case 'x':
+    case 'z':
+    cout<<"Consonant";        
+
+}
+
+
+
+
+
+
     return 0;
 }
