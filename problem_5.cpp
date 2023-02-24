a6f200be139bce29e33d9b191e67d055063e4f4d
221029cs06 <113498631+221029cs06@users.noreply.github.com>
2023-02-24 22:49:40 +0300

    Create problem_5.cpp

---
+++ problem_5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a;
+   cin>>a;
+   if(a%3==0 && a%4==0){
+       cout<<"Yes"<<endl;
+       
+ 
+   }
+   
+   
+   
+   else{
+       cout<<"No";
+   }
+   
+
+    return 0;
+}
